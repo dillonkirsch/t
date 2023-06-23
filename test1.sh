def227adef954106c3a81bbed9191d69633bed73
Dillon Kirsch <dillon@dillonkirsch.com>
2023-06-23 07:55:04 -0600

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,21 +1,30 @@
-deepspeed train_freeform.py \
-    --model_name_or_path /workspace/llama-7b/ \
-    --data_path /workspace//WizardLM_alpaca_evol_instruct_70k_unfiltered/WizardLM_alpaca_evol_instruct_70k_unfiltered.json \
-    --output_dir /workspace/final_out/ \
-    --num_train_epochs 3 \
-    --model_max_length 2048 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 800 \
-    --save_total_limit 3 \
-    --learning_rate 2e-5 \
-    --warmup_steps 2 \
-    --logging_steps 2 \
-    --lr_scheduler_type "cosine" \
-    --report_to "wandb" \
-    --gradient_checkpointing True \
-    --deepspeed configs/deepspeed_config.json \
-    --fp16 True
+source /workspace/venv/bin/activate
+
+export NCCL_P2P_DISABLE=1
+export NCCL_P2P_LEVEL=LOC
+
+torchrun --nproc_per_node=8 --master_port=20001 /workspace/FastChat/fastchat/train/train_mem.py \
+  --model_name_or_path /workspace/llama-7b \
+  --data_path /workspace/as_conversations.json \
+  --bf16 True \
+  --output_dir /workspace/airoboros-uncensored-13b \
+  --num_train_epochs 3 \
+  --per_device_train_batch_size 20 \
+  --per_device_eval_batch_size 20 \
+  --gradient_accumulation_steps 2 \
+  --evaluation_strategy "steps" \
+  --eval_steps 500 \
+  --save_strategy "steps" \
+  --save_steps 500 \
+  --save_total_limit 10 \
+  --learning_rate 2e-5 \
+  --weight_decay 0. \
+  --warmup_ratio 0.04 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --fsdp "full_shard auto_wrap offload" \
+  --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+  --tf32 True \
+  --model_max_length 2048 \
+  --gradient_checkpointing True \
+  --lazy_preprocess True
