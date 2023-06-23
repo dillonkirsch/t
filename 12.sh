a02977b8051c29a6048eabdc1a98b9903c81391a
Dillon Kirsch <dillon@dillonkirsch.com>
2023-06-22 19:32:54 -0600

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -8,8 +8,10 @@ else
     dataset_download=$3
 fi
 echo "Please Run:"
-echo "shell=BASH && wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh -O ~/anaconda3.sh && bash ~/anaconda3.sh -b -p $HOME/anaconda3 && /root/anaconda3/bin/conda init &&source /root/.bashrc"
-
+echo "shell=BASH && wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh -O ~/anaconda3.sh && bash ~/anaconda3.sh -b -p $HOME/anaconda3 && /root/anaconda3/bin/conda init && source /root/.bashrc"
+shell=BASH && wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh -O ~/anaconda3.sh && bash ~/anaconda3.sh -b -p $HOME/anaconda3 && /root/anaconda3/bin/conda init && source /root/.bashrc
+conda deactivate
+conda config --set auto_activate_base false
 apt install sudo -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt update
